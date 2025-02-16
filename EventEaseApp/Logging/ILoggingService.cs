123b590fd96a32d4191f144dfcc4e17cd774c6c4
thereigo <ig.pozdnakovs@gmail.com>
2025-02-16 22:12:32 +0200

    Update ILoggingService to allow nullable message parameter in LogException method

--- EventEaseApp/Logging/ILoggingService.cs
+++ EventEaseApp/Logging/ILoggingService.cs
@@ -2,7 +2,7 @@ namespace EventEaseApp.Logging
 {
     public interface ILoggingService
     {
-        void LogException(Exception ex, string message = null);
+        void LogException(Exception ex, string? message = null);
         void LogInformation(string message);
     }
 }
